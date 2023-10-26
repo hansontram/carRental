426e92c67709639ace208a6c47adc69c13bc286c
Hanson Tram <htram.dev@gmail.com>
2023-10-26 15:55:36 -0700

    Set up script file

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ window.onload = function() {
     estimateButton.onclick = estimateButtonClicked;
 
 }
+
+function estimateButton(){
+    const rental = {};
+
+    // get the details
+    rental.pickupDate = document.getElementById("pickupDateInput").value;
+
+    // get the options
+
+    // get under 25 details
+}
